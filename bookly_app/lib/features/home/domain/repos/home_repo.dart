41df6e03fbc0c3816ebf353167beec0aebba6aed
radwaadel17 <radwaadel959@gmail.com>
2025-02-04 22:47:53 +0200

    create faluire model , home repo

---
+++ bookly_app/lib/features/home/domain/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/core/errors/faluire.dart';
+import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Faluire , List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Faluire , List<BookEntity>>> fetchNewestBooks();
+}
