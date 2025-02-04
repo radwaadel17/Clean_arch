12341420337076be93faf14656d14afd20faec50
radwaadel17 <radwaadel959@gmail.com>
2025-02-04 22:44:32 +0200

    create book entity

---
+++ bookly_app/lib/features/home/domain/entites/book_entity.dart
@@ -0,0 +1,8 @@
+class BookEntity {
+  final String image ;
+  final String title ;
+  final String author ;
+  final num price ;
+  final num rating ;
+  BookEntity({required this.author , required this.image , required this.price , required this.rating  , required this.title});
+}
