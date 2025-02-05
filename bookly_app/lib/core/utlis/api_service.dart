f0cd0188c362071b547585a0bdc136846d4e4beb
radwaadel17 <radwaadel959@gmail.com>
2025-02-05 22:45:19 +0200

    create api service class

---
+++ bookly_app/lib/core/utlis/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio ;
+  ApiService(this.dio);
+  final baseUrl = 'https://www.googleapis.com/books/v1/volumes?';
+  Future<Map<String , dynamic>> getData(String endPoint) async{
+    Response response = await dio.get('$baseUrl$endPoint');
+    Map<String , dynamic > responseData = response.data ;
+    return responseData;
+  }
+
+}
