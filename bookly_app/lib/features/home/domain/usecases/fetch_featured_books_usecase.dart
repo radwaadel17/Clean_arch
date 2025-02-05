878feed8c3e2662a9b4277bb31d9c2c3798d4840
radwaadel17 <radwaadel959@gmail.com>
2025-02-05 21:54:42 +0200

    create fetch featured books use case

---
+++ bookly_app/lib/features/home/domain/usecases/fetch_featured_books_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/core/errors/faluire.dart';
+import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUsecase implements UseCase<List<BookEntity> ,  NoParam>{
+  final HomeRepo homeRepo ;
+  FetchFeaturedBooksUsecase(this.homeRepo);
+  
+  @override
+  Future<Either<Faluire, List<BookEntity>>> call(NoParam? param) {
+    // say check premission 
+    return homeRepo.fetchFeaturedBooks();
+  }
+  
+}
+abstract class UseCase<Type , Param> {
+  Future<Either<Faluire , Type>> call(Param param);
+}
+
+class NoParam{}
