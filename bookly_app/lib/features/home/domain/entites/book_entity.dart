5fb38d54edb07ae13678d7b347deaea39bc64dc6
radwaadel17 <radwaadel959@gmail.com>
2025-02-06 22:33:24 +0200

    generate book entity adapter

--- bookly_app/lib/features/home/domain/entites/book_entity.dart
+++ bookly_app/lib/features/home/domain/entites/book_entity.dart
@@ -1,12 +1,21 @@
-class BookEntity {
+import 'package:hive/hive.dart';
+
+
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
+class BookEntity extends HiveObject {
+  @HiveField(0)
   final String? image;
+  @HiveField(1)
   final String? title;
+  @HiveField(2)
   final String? author;
+  @HiveField(3)
   final num? price;
-  final String? bookId ;
+  @HiveField(4)
+  final String? bookId;
   BookEntity(
-      {
-      required this.bookId,  
+      {required this.bookId,
       required this.author,
       required this.image,
       required this.price,
---
+++ bookly_app/lib/features/home/domain/entites/book_entity.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'book_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class BookEntityAdapter extends TypeAdapter<BookEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  BookEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return BookEntity(
+      bookId: fields[4] as String?,
+      author: fields[2] as String?,
+      image: fields[0] as String?,
+      price: fields[3] as num?,
+      title: fields[1] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, BookEntity obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.image)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.author)
+      ..writeByte(3)
+      ..write(obj.price)
+      ..writeByte(4)
+      ..write(obj.bookId);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BookEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
