c72553ce47ed7b103ad14b539e3965b3dd3a97b5
radwaadel17 <radwaadel959@gmail.com>
2025-02-06 22:35:59 +0200

    register adapter and open box

--- bookly_app/lib/main.dart
+++ bookly_app/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utlis/approuter.dart';
+import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main()async {
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox('BookEntity');
   runApp(const BooklyApp());
 }
 
