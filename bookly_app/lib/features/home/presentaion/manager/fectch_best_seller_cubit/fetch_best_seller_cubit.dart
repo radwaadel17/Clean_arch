f1414ea44e2ce87920e95d9ffb9a1c53010bd95d
radwaadel17 <radwaadel959@gmail.com>
2025-02-11 23:01:15 +0200

    fetch_best_seller_cubit

---
+++ bookly_app/lib/features/home/presentaion/manager/fectch_best_seller_cubit/fetch_best_seller_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:bookly_app/features/home/presentaion/manager/fectch_best_seller_cubit/fetch_best_seller_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FetchBestSellerCubit extends Cubit<FetchBestSellerStates> {
+  FetchBestSellerCubit(this.homeRepo) : super(FetchBestSellerStatesInit());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchBestSellerMethod() async {
+    emit(FetchBestSellerStatesLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((faluire) {
+      emit(FetchBestSellerStatesFaluire(faluire.errorMessage));
+    }, (books) {
+      emit(FetchBestSellerStatesSucsess());
+    });
+  }
+}
