2c6f2f8d89684231a18dd4a6e5b85fc2e4788fe8
radwaadel17 <radwaadel959@gmail.com>
2025-02-15 20:28:18 +0200

    reach to 70% of the list

--- bookly_app/lib/features/home/presentaion/views/widgets/ListViewHorizontal.dart
+++ bookly_app/lib/features/home/presentaion/views/widgets/ListViewHorizontal.dart
@@ -1,10 +1,40 @@
 import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
+import 'package:bookly_app/features/home/presentaion/manager/fetch_best_seller_cubit/fetch_best_seller_cubit.dart';
 import 'package:bookly_app/features/home/presentaion/views/widgets/CustomBookImage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ListViewHorizontal extends StatelessWidget {
+class ListViewHorizontal extends StatefulWidget {
   const ListViewHorizontal({super.key, required this.books});
   final List<BookEntity> books;
+
+  @override
+  State<ListViewHorizontal> createState() => _ListViewHorizontalState();
+}
+
+class _ListViewHorizontalState extends State<ListViewHorizontal> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent * 0.7) {
+        BlocProvider.of<FetchBestSellerCubit>(context).fetchBestSellerMethod(pagenumber: 0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +43,13 @@ class ListViewHorizontal extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.only(left: 16),
         child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: books.length,
-            itemBuilder: (context, index) {
-              return CustomBookImage(url: books[index].image,);
-            }),
+          controller: _scrollController,
+          scrollDirection: Axis.horizontal,
+          itemCount: widget.books.length,
+          itemBuilder: (context, index) {
+            return CustomBookImage(url: widget.books[index].image);
+          },
+        ),
       ),
     );
   }
